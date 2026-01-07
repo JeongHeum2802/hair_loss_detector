cd0a8837448d732999968aebe3eed93be5ac9f70
wamddu <yhy6923@naver.com>
2026-01-07 23:25:06 +0900

    백엔드 회원가입, 로그인 기능 구현

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose  = require('mongoose');
+const http = require('http');
+
+const userRoutes = require('./routes/user');
+
+app.use(express.json());
+app.use('/main', userRoutes);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("몽고디비 연결 성공!");
+    app.listen(3000);
+    console.log("서버 연결 성공!");
+}).catch(err => {
+    console.log(err);
+})
