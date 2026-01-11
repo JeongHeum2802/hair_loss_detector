9be4d7474a761f17fd7935bfe028c2780b4753d7
ohgunseo <okko6513@gmail.com>
2026-01-11 21:41:43 +0900

    수정본

--- frontend/src/components/RecordList.jsx
+++ frontend/src/components/RecordList.jsx
@@ -1,6 +1,29 @@
+import { useContext, useState, useEffect } from 'react';
 import RecordCard from './RecordCard';
+import AuthContext from '../store/auth-context';
 
-const RecordList = ({historyRecords}) => {
+const RecordList = () => {
+    const [records, setRecords] = useState([]);
+    const authcontext = useContext(AuthContext);
+    useEffect(() => {
+        const fetchRecords = async() => {
+        const response = await fetch("http://localhost:3000/predict/viewRecords/", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({accessToken: authcontext.token})
+        }) ;
+        if (response.ok){
+            const resdata = await response.json();
+            setRecords(resdata);
+        }
+        else {
+            alert("통신에 문제가 생겼습니다.");
+        }
+    }
+    fetchRecords();
+    }, []);
     return (
         <section className="mt-10">
             <h3 className="text-xl font-bold text-slate-800 mb-6 flex items-center gap-2">
@@ -9,7 +32,7 @@ const RecordList = ({historyRecords}) => {
             </h3>
 
             <div className="space-y-4 max-h-[500px] overflow-y-auto pr-2 custom-scrollbar">
-                {historyRecords.map(record => (
+                {records.map(record => (
                     <RecordCard
                         key={record._id}
                         record={record}
